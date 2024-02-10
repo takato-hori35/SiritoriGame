068a13463a9b00a8539049837657f0a313b3d1a5
takato hori <iuyda@icloud.com>
2024-02-10 16:56:24 +0900

    c#っぽい書き方がしたいのでloop文変更

--- Program.cs
+++ Program.cs
@@ -6,15 +6,12 @@ class Shiritori
     Console.WriteLine("しりとりスタート！");
     Console.WriteLine("最初の単語を入力してください");
     string lastWord = Console.ReadLine();
+    string nextWord; // while条件で使うため先に定義
     char lastChar = lastWord[lastWord.Length - 1];
 
-    while(true) {
+    do {
       Console.WriteLine($"{lastChar}で始まる単語:");
-      string nextWord = Console.ReadLine();
-      if(nextWord == "終了") {
-        Console.WriteLine("しりとりゲームを終了します。");
-        break;
-      }
+      nextWord = Console.ReadLine();
 
       if(nextWord.EndsWith("ん")) {
         Console.WriteLine("ゲームオーバー: 単語が「ん」で終わっています。");
@@ -22,12 +19,14 @@ class Shiritori
       }
 
       if(lastChar != nextWord[0]) {
-        Console.WriteLine($"入力エラー: '{lastChar}'で始まる単語を入力してください。");
+        Console.WriteLine($"ゲームオーバー: '単語が{lastChar}'で始まっていません。");
         continue;
       }
 
       lastChar = nextWord[nextWord.Length - 1]; // 次の単語の最後の文字を更新
-    }
+    } while(nextWord != "終了");
+
+    Console.WriteLine("しりとりを終了します。");
   }
 }
 
